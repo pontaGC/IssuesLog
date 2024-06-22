45992dbe9739c19dacd539feff06cfd614cb561e
pontaGC <makizawawa@gmail.com>
2024-06-23 07:49:27 +0900

    Refactor by Lombok.

--- Java/Ilog/src/main/java/com/example/Ilog/domain/issue/IssueEntity.java
+++ Java/Ilog/src/main/java/com/example/Ilog/domain/issue/IssueEntity.java
@@ -1,56 +1,18 @@
 package com.example.Ilog.domain.issue;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+/*
+ Lombokのアノテーションの詳細は以下サイト参照
+ https://projectlombok.org/features/
+ 構造ウィンドウ (「Alt + 7」)でコンストラクタとgetter/setterなどが自動生成されていることが確認できる
+*/
+
+@AllArgsConstructor
+@Data
 public class IssueEntity {
-
-    //region Fields
-
     private long id;
     private String summary;
     private  String description;
-
-    //endregion
-
-    //region Constructors
-
-    /**
-     * Initializes a new instance of this class.
-     * @param id the issue ID.
-     * @param summary the issue summary.
-     * @param description the issue description.
-     */
-    public IssueEntity(long id, String summary, String description) {
-        this.id = id;
-        this.summary = summary;
-        this.description = description;
-    }
-
-    //endregion
-
-    //region Getters / Setters
-
-    public long getId() {
-        return id;
-    }
-
-    public void setId(long id) {
-        this.id = id;
-    }
-
-    public String getSummary() {
-        return summary;
-    }
-
-    public void setSummary(String summary) {
-        this.summary = summary;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
-    //endregion
 }
