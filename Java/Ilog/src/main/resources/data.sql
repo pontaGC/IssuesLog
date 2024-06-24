32c7338da0f3776086cbbd4c780b91caa0f2ca58
pontaGC <makizawawa@gmail.com>
2024-06-24 22:32:10 +0900

    Add issues data definition and initial issues data

---
+++ Java/Ilog/src/main/resources/data.sql
@@ -0,0 +1,4 @@
+--idは自動採番のため、記述は不要
+insert into issues(summary, description) values('バグA', 'バグがあります。');
+insert into issues(summary, description) values('機能要望B', 'Bに機能が欲しいです。');
+insert into issues(summary, description) values('画面Cが遅い', '早くしてほしいです。');
