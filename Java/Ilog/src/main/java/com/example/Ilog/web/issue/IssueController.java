378e00c2cb467ff7da5155b8a86076e993ddb586
pontaGC <makizawawa@gmail.com>
2024-06-25 13:28:23 +0900

    Add form to create issues.

--- Java/Ilog/src/main/java/com/example/Ilog/web/issue/IssueController.java
+++ Java/Ilog/src/main/java/com/example/Ilog/web/issue/IssueController.java
@@ -5,18 +5,27 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
 
+//@RequestMappingは共通のパスを定義
+
 @Controller
+@RequestMapping("/issues")
 @RequiredArgsConstructor
 public class IssueController {
 
     private  final IIssueService issueService;
 
-    @GetMapping("/issues")
+    @GetMapping
     public  String showList(Model model){
         model.addAttribute("issueList", this.issueService.findAll());
         return "issues/list";
+    }
+
+    @GetMapping("/creationForm")
+    public  String showCreationForm(){
+        return "issues/creationForm";
     }
 }
